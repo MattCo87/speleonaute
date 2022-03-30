63e035c95b5efc6c2060d9586c7b42b294aa80c4
Callipolitis <yanis.callipolitis@gmail.com>
2022-03-30 18:24:58 +0200

    controlleur combat

---
+++ src/Controller/MoteurCombatController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MoteurCombatController extends AbstractController
+{
+    /**
+     * @Route("/moteur/combat", name="app_moteur_combat")
+     */
+    public function index(): Response
+    {
+        return $this->render('moteur_combat/index.html.twig', [
+            'controller_name' => 'MoteurCombatController',
+        ]);
+    }
+}
