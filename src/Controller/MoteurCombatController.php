cd7877c3a27532f6d994ac54dc31c4c71b970eda
Joel Obrecht <joel.obrecht@gmail.com>
2022-03-31 14:42:45 +0200

    Premiers tests

--- src/Controller/MoteurCombatController.php
+++ src/Controller/MoteurCombatController.php
@@ -2,19 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\DBAL\Query\QueryBuilder;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManager;
+use App\Repository\CreatureRepository;
+use App\Entity\Creature;
 
 class MoteurCombatController extends AbstractController
 {
     /**
      * @Route("/moteur/combat", name="app_moteur_combat")
      */
-    public function index(): Response
+    public function MoteurTest(ManagerRegistry $doctrine): Response
     {
-
-        $res = array();
+        $id = 1;
+        $query = $doctrine->getRepository(Creature::class)->findAll();
+        dd($query);
+        $res = "";
 
         dd($res);
         return $this->render('moteur_combat/index.html.twig', [
