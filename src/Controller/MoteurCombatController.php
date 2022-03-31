85311d28e2cd86de86a8c101a4b3991d9128e9e1
Callipolitis <yanis.callipolitis@gmail.com>
2022-03-31 21:53:59 +0200

    aze

--- src/Controller/MoteurCombatController.php
+++ src/Controller/MoteurCombatController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManager;
 use Doctrine\DBAL\Query\QueryBuilder;
+use PhpParser\Node\Stmt\Foreach_;
 
 class MoteurCombatController extends AbstractController
 {
@@ -23,6 +24,8 @@ class MoteurCombatController extends AbstractController
         // new MessageGenerator and pass it to you!
         // ...
        // $id = 1;
+
+       //Tableau Hote
         $tableauHote = array();
         //$tiersCrea = $creatureFormation->getRepository(CreatureFormation::class)->findAll();
         //$tiersCrea = array();
@@ -34,7 +37,44 @@ class MoteurCombatController extends AbstractController
        // $tableauHote = $idCrea->getNom();
         array_push($tableauHote, $idCrea->getId());
        }
-       dd($tableauHote);
+      // dd($tableauHote);
+
+
+      //Tableau monstre
+       $id = 2;
+       $tableauMonstre = array();
+       //$tiersCrea = $creatureFormation->getRepository(CreatureFormation::class)->findAll();
+       //$tiersCrea = array();
+       $tiersCrea2 = $doctrine->getRepository(CreatureFormation::class)->findBy(['lienFormation' => $id]);
+      // $nom = $tiersCrea->getNom();
+      //dd($tiersCrea);
+      foreach($tiersCrea2 as $crea){
+          $idCrea2 = $crea->getlienCreature();
+      // $tableauHote = $idCrea->getNom();
+       array_push($tableauMonstre, $idCrea2->getId());
+      }
+      //dd($tableauMonstre);
+
+    
+
+
+      //TableauCreature
+         $tableauCreature = array();
+        foreach($tableauHote as $hote){
+        
+        $creature = $doctrine->getRepository(Creature::class)->findBy(['id' => $hote]);
+            foreach($creature as $crea){
+                $idCrea2 = $crea->getNom();
+               // dd($idCrea2);
+            // $tableauHote = $idCrea->getNom();
+            $Creature2['nom'] = $crea->getNom();
+            $Creature2['niveau'] = $crea->getNiveau();
+            $Creature2['exp'] = $crea->getExp();
+            dd($Creature2);
+            }
+        dd($creature);
+       // $tableauCreature[0]['nom' => $creature->getNom()];
+        $nom = $creature->getNom();
 
 
 
@@ -53,16 +93,7 @@ class MoteurCombatController extends AbstractController
         ]);
     }
 
-
-
-
-
-
-
-
-
-
-
+    }
 
 
 
