9b74bfcbbde246be17d09c2183a1c1e570d9ea79
MattCo87 <77285498+MattCo87@users.noreply.github.com>
2022-03-31 11:31:29 +0200

    controller Formation

--- src/Form/CreatureFormationType.php
+++ src/Form/CreatureFormationType.php
@@ -8,14 +8,12 @@ use App\Entity\Creature;
 use App\Repository\FormationRepository;
 use App\Repository\CreatureFormationRepository;
 use App\Repository\CreatureRepository;
-
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
 use Symfony\Component\Security\Core\Security as CoreSecurity;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -34,68 +32,38 @@ class CreatureFormationType extends AbstractType
     {
         $builder
             // On affiche la liste des Formations
-            /*
             ->add('lienFormation', EntityType::class, array(
                 'class' => Formation::class,
-                'choice_label' => 'nom',
-            ))
-            */
-
-            ->add('lienFormation', EntityType::class, array(
-                'class' => Formation::class,
-
                 'query_builder' => function (FormationRepository $er) {
                     return $er->createQueryBuilder('c')
                         ->where('c.lienUser = :val')
                         ->setParameter('val', $this->security->getUser());
                 },
-
                 'choice_label' => 'nom',
             ))
 
 
             // On affiche la liste des Créatures
-            /*
             ->add('lienCreature', EntityType::class, array(
                 'class' => Creature::class,
-                'choice_label' => 'nom',
-            ))
-            */
-
-            ->add('lienCreature', EntityType::class, array(
-                'class' => Creature::class,             
-                
                 'query_builder' => function () {
                     // Une sous requête affichant la liste des personnages appartenant à une formation
                     $ecf = new CreatureFormationRepository($this->registry);
                     $subQueryBuilder = $ecf->createQueryBuilder('cf');
                     $subQuery = $subQueryBuilder
-                        ->select('IDENTITY(cf.lienCreature)')            
-                    ;
+                        ->select('IDENTITY(cf.lienCreature)');
 
                     // Une requête retournant les personnages appartenants à l'utilisateur et qui ne sont pas dans une formation
                     $er = new CreatureRepository($this->registry);
                     $queryBuilder = $er->createQueryBuilder('c');
                     $query = $queryBuilder
                         ->where($queryBuilder->expr()->notIn('c.id', $subQuery->getDQL()))
-                        ->andwhere('c.lienUser = :val')                       
-                        ->setParameter('val', $this->security->getUser())    
-                    ;
-                    
-                    return $query;           
+                        ->andwhere('c.lienUser = :val')
+                        ->setParameter('val', $this->security->getUser());
+                    return $query;
                 },
-
                 'choice_label' => 'nom',
-
             ))
-
-
-
-
-
-
-
-
 
             // On choisit sa localisation
             ->add('localisation', ChoiceType::class, array(
@@ -117,11 +85,8 @@ class CreatureFormationType extends AbstractType
             ])
 
             // Bouton pour valider la création de la CreationFormation
-            ->add(
-                'submit',
-                SubmitType::class,
-                [
-                    'label' => 'OK'
+            ->add('submit', SubmitType::class, [
+                    'label' => 'OK',
                 ],
             );
     }
