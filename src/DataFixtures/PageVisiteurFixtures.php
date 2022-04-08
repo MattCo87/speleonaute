bcaa9cb98d2449621b6d45391eefeaa2f31c40cb
Joel Obrecht <joel.obrecht@gmail.com>
2022-04-08 17:42:21 +0200

    Corrections bug reste routes bizarre

--- src/DataFixtures/PageVisiteurFixtures.php
+++ src/DataFixtures/PageVisiteurFixtures.php
@@ -24,9 +24,9 @@ class PageVisiteurFixtures extends Fixture implements OrderedFixtureInterface
         <<<HTML
         <h1>Glossaire</h1>
         <h2>Hôte</h2>
-        <P>Ce sont des monstres de la FOsse, mais "apprivoisés" et accoutumés à la vie à la surface grâce à un greffon</p>
+        <P>Ce sont des monstres de la Fosse, mais "apprivoisés" et accoutumés à la vie à la surface grâce à un greffon.</p>
         <h2>Monstre</h2>
-        <P>Ce sont les habitants de la fFosse. Il y en a de toutes les sortes, notamment à cause du courant, qui permet à la lumière de pénétrer très profondément dans la Fosse et donc de recréer des habitats entiers propores à leur croissance.</p>
+        <P>Ce sont les habitants de la Fosse. Il y en a de toutes les sortes, notamment à cause du courant, qui permet à la lumière de pénétrer très profondément dans la Fosse et donc de recréer des habitats entiers propres à leur croissance.</p>
         <h2>Chevalement</h2>
         <P>On ne peut pas le rater, il s'agit de la grande construction au-dessus de la Fosse. Hobb, son maître, décide à quel niveau vous avez droit d'aller. C'est lui qui gère les treuils qui envoient et remontent les cages d'exploration.</p>
         HTML];
@@ -34,7 +34,7 @@ class PageVisiteurFixtures extends Fixture implements OrderedFixtureInterface
             "titre" => "Règles", "contenu" =>
             <<<HTML
         <h1>Mentions légales</h1>
-        <p>Merci de lire avec attention les différentes modalités d’utilisation du présent site avant d’y parcourir ses pages. En vous connectant sur ce site, vous acceptez sans réserves les présentes modalités.</p>
+        <p>Merci de lire avec attention les différentes modalités d’utilisation du présent site avant d’en parcourir les pages. En vous connectant sur ce site, vous acceptez sans réserves les présentes modalités.</p>
         <p>Aussi, conformément à l’article n°6 de la Loi n°2004-575 du 21 Juin 2004 pour la confiance dans l’économie numérique, les responsables du présent site internet www.speleonautes.fr sont :</p>
             <h2>Éditeur du Site :<h2>
             <p>Nom : Joel Yanis Matthieu Prénom : JYM Limoges Email : speleonautes@gmail.com Site Web : www.speleonautes.fr</p>
@@ -51,20 +51,20 @@ class PageVisiteurFixtures extends Fixture implements OrderedFixtureInterface
             <<<HTML
         <h1>Règles du jeu</h1>
         <h2>Comment y jouer ?</h2>
-        <P>Le jeu consiste à sélectionner une équipe d'hôtes, en fonction des actions et stratégies propres à chaque hôte, puis de les envoyer affronter, dans la Fosse, des hordes de monstres. Le combat est automatique, et vous aurez accès une fois fini à un log détaillé permettant de comprendre ce qui s'est passé</p>
+        <P>Le jeu consiste à sélectionner une équipe d'hôtes, en fonction des actions et stratégies propres à chaque hôte, puis de les envoyer affronter, dans la Fosse, des hordes de monstres. Le combat est automatique, et vous aurez accès une fois fini à un log détaillé permettant de comprendre ce qui s'est passé.</p>
         <p>Vous ne perdrez pas vos hôtes, ils seront secourus une fois arrivés à 0 points de vie, et, moyennant un temps d'attente, ils seront à nouveau prêts pour être envoyés à un aute combat.</p>
         <h2>Stratégies</h2>
         <p>Le jeu consiste à choisir les bons hôtes à envoyer au combat, selon les monstres qu'ils auront à affronter, et leurs stratégies. Il est souvent nécessaire d'envoyer une première fois une équipe pour voir comment les monstres opèrent et en déduire, lors d'un second combat, votre stratégie.</p>
         <h2>Évolution</h2>
-        <p>Au fur et à mesure des vombats, vos hôtes vont gagner en expérience et évoluer, mais aussi vous aller accéder à plus d'hôtes, qui vous permettront de faire plusieurs combats en parallèle.</p>
-        <p>Avwec votre progression, vous allez pouvoir accéder à des niveaux différents de la Fosse, qui mettront plus de temps à atteindre, mais aussi rapporteront bien plus que les niveaux proches de la surface.</p>
+        <p>Au fur et à mesure des combats, vos hôtes vont gagner en expérience et évoluer, mais aussi vous aller accéder à plus d'hôtes, qui vous permettront de faire plusieurs combats en parallèle.</p>
+        <p>Avec votre progression, vous allez pouvoir accéder à des niveaux différents de la Fosse, qui mettront plus de temps à atteindre, mais aussi rapporteront bien plus que les niveaux proches de la surface.</p>
         HTML
         ];
         $pages["PG_team"] = [
             "titre" => "Team", "contenu" =>
             <<<HTML
         <p><strong>Yanis</strong> : l'idée originale vient d'un jeu auquel il a beaucoup joué, aujourd'hui disparu ! Le moteur de combat, c'est lui.</p>
-        <p><strong>Matthieu</strong> : Notre référent technique en Symfony, toujours prêt pour un petit <code>php bin/console<code> !</p>
+        <p><strong>Matthieu</strong> : Notre référent technique en Symfony, toujours prêt pour un petit <code>php bin/console</code> !</p>
         <p><strong>Joël</strong> : Sa passion pour <em>Made in Abyss</em> lui a donné envie de créer un univers parallèle avec une Fosse et des monstres ;)</p>
         HTML
         ];
